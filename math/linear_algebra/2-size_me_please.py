de59351c939a89ec42d6a2cd38179692fc72e481
jicruz96 <accounts@jicruz.com>
2023-07-08 01:48:34 -0400

    inital commit task 2275-2

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+
+def matrix_shape(matrix):
+    """Compute the shape of a matrix.
+
+    * You can assume all elements in the same dimension are of the same type
+        and shape
+
+    Returns:
+        Shape of each dimension as a list of integers.
+    """
+    dimensions = []
+    dimension = len(matrix)
+    dimensions.append(dimension)
+    if dimension > 0 and isinstance(matrix[0], list):
+        next_dimensions = matrix_shape(matrix[0])
+        dimensions.extend(next_dimensions)
+    return dimensions
+
+
+if __name__ == "__main__":
+    mat1 = [[1, 2], [3, 4]]
+    print(matrix_shape(mat1))
+    mat2 = [
+        [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15]],
+        [[16, 17, 18, 19, 20], [21, 22, 23, 24, 25], [26, 27, 28, 29, 30]],
+    ]
+    print(matrix_shape(mat2))
