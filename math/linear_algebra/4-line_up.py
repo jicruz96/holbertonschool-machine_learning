acfff45b27fd21e8a9260a19ecf1dfe7d38bf510
jicruz96 <accounts@jicruz.com>
2023-07-08 02:10:03 -0400

    inital commit task 2275-4

---
+++ math/linear_algebra/4-line_up.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+
+def add_arrays(arr1, arr2):
+    """
+    adds two same-length lists element-wise.
+
+    If lists are not the same length, returns None.
+    """
+    if len(arr1) != len(arr2):
+        return None
+    return [arr1[i] + arr2[i] for i in range(len(arr1))]
+
+
+if __name__ == "__main__":
+    arr1 = [1, 2, 3, 4]
+    arr2 = [5, 6, 7, 8]
+    print(add_arrays(arr1, arr2))
+    print(arr1)
+    print(arr2)
+    print(add_arrays(arr1, [1, 2, 3]))
