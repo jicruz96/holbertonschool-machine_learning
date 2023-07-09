08705a5c75ad2e5e90ef9ac3e7b556b42c09c97c
jicruz96 <accounts@jicruz.com>
2023-07-09 00:52:59 -0400

    non numpy version of 102

--- math/linear_algebra/102-squashed_like_sardines.py
+++ math/linear_algebra/102-squashed_like_sardines.py
@@ -1,23 +1,20 @@
 #!/usr/bin/env python3
 
-import time
-
-import numpy as np
-
 
 def cat_matrices(mat1, mat2, axis=0):
     """
-    Concatenates two matrices along a specific axis.
-
-    Args:
-        mat1 (list of lists): first given matrix
-        mat2 (list of lists): second given matrix
-        axis (int): axis to concatenate along
-
-    Returns:
-        list of lists: concatenated matrix
+    Concatenates two matrices of any dimension along a specific axis
     """
-    try:
-        return np.concatenate((np.array(mat1), np.array(mat2)), axis=axis)
-    except ValueError:
-        return None
+    if axis == 0:
+        if len(mat1[0]) != len(mat2[0]):
+            return None
+        return [i.copy() for i in mat1] + [i.copy() for i in mat2]
+    if axis == 1:
+        if len(mat1) != len(mat2):
+            return None
+        return [mat1[i].copy() + mat2[i].copy() for i in range(len(mat1))]
+    if axis == 2:
+        if len(mat1) != len(mat2):
+            return None
+        return [cat_matrices(mat1[i], mat2[i]) for i in range(len(mat1))]
+    return None
