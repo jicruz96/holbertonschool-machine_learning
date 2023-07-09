60c8e9ef6e1ea65a5780501fb07bb0602b0b9793
jicruz96 <accounts@jicruz.com>
2023-07-09 01:09:29 -0400

    remove imports from exercise 2275-10 (not allowed)

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -3,23 +3,8 @@
 Module for exercise number 10 for the numpy and linear algebra
 project (https://intranet.hbtn.io/projects/2275)
 """
-import numpy as np
 
 
 def np_shape(matrix):
     """calculates the shape of a numpy.ndarray"""
     return matrix.shape
-
-
-if __name__ == "__main__":
-    mat1 = np.array([1, 2, 3, 4, 5, 6])
-    mat2 = np.array([])
-    mat3 = np.array(
-        [
-            [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]],
-            [[11, 12, 13, 14, 15], [16, 17, 18, 19, 20]],
-        ]
-    )
-    print(np_shape(mat1))
-    print(np_shape(mat2))
-    print(np_shape(mat3))
