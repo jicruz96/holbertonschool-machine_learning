92ae4ee11e95b94b3676c84d86cd4ca6ee63cd95
jicruz96 <accounts@jicruz.com>
2023-07-09 01:16:04 -0400

    remove imports from 2275-100 (not allowed)

--- math/linear_algebra/100-slice_like_a_ninja.py
+++ math/linear_algebra/100-slice_like_a_ninja.py
@@ -4,8 +4,6 @@ Module for exercise number 100 for the numpy and linear algebra
 project (https://intranet.hbtn.io/projects/2275)
 """
 
-import numpy as np
-
 
 def np_slice(matrix, axes={}):
     """
@@ -19,18 +17,3 @@ def np_slice(matrix, axes={}):
     DEFAULT_SLICE = slice(None, None, None)  # slice to select everything
     slices = [slice(*axes.get(i, DEFAULT_SLICE)) for i in range(matrix.ndim)]
     return matrix[tuple(slices)]
-
-
-if __name__ == "__main__":
-    mat1 = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
-    print(np_slice(mat1, axes={1: (1, 3)}))
-    print(mat1)
-    mat2 = np.array(
-        [
-            [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]],
-            [[11, 12, 13, 14, 15], [16, 17, 18, 19, 20]],
-            [[21, 22, 23, 24, 25], [26, 27, 28, 29, 30]],
-        ]
-    )
-    print(np_slice(mat2, axes={0: (2,), 2: (None, None, -2)}))
-    print(mat2)
