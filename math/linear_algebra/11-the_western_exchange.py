0ae0ecf880973d3874e2e13e427f849f8ad029f8
jicruz96 <accounts@jicruz.com>
2023-07-09 01:13:25 -0400

    remove imports from 2275-11 (not allowed)

--- math/linear_algebra/11-the_western_exchange.py
+++ math/linear_algebra/11-the_western_exchange.py
@@ -4,26 +4,7 @@ Module for exercise number 11 for the numpy and linear algebra
 project (https://intranet.hbtn.io/projects/2275)
 """
 
-import numpy as np
-
 
 def np_transpose(matrix):
     """transposes a numpy matrix."""
     return np.transpose(matrix)
-
-
-if __name__ == "__main__":
-    mat1 = np.array([1, 2, 3, 4, 5, 6])
-    mat2 = np.array([])
-    mat3 = np.array(
-        [
-            [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]],
-            [[11, 12, 13, 14, 15], [16, 17, 18, 19, 20]],
-        ]
-    )
-    print(np_transpose(mat1))
-    print(mat1)
-    print(np_transpose(mat2))
-    print(mat2)
-    print(np_transpose(mat3))
-    print(mat3)
